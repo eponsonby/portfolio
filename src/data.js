f3bb3926e4aecc96693639fb140709eb96dec1e4
Erin Ponsonby <erinponsonby@gmail.com>
2022-09-03 09:41:20 -0600

    Add scrolling gifs to projects component

--- src/data.js
+++ src/data.js
@@ -1,27 +1,27 @@
 export const projects = [
     {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
+      title: "Sparkle Smash",
+      subtitle: "JavaScript",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
+      image:"https://api.apify.com/v2/key-value-stores/NbtooQUI47jw0jSD0/records/sparkle-smash.herokuapp.com-scroll_lossy-comp",
+      link: "https://sparkle-smash.herokuapp.com/",
     },
     {
-      title: "React Tracks",
-      subtitle: "React and Python",
+      title: "The Trailhead",
+      subtitle: "React and Ruby on Rails",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
+      image: "https://api.apify.com/v2/key-value-stores/o85tr5HRPH9qFQcM9/records/thetrailhead.herokuapp.com-scroll_original",
+      link: "https://thetrailhead.herokuapp.com/",
     },
     {
-      title: "DevChat",
+      title: "WunderTrip",
       subtitle: "React and Firebase",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
+      image: "https://api.apify.com/v2/key-value-stores/lwo4bkSkdeUOZJRwb/records/wundertrip.herokuapp.com-scroll_original",
+      link: "https://wundertrip.herokuapp.com/",
     },
     {
       title: "Epic Todo App",
@@ -32,3 +32,25 @@ export const projects = [
       link: "https://pythonbootcamp.com",
     },
   ];
+
+  export const skills = ["JavaScript", "React", "Next.js", "Ruby on Rails", "TailwindCSS", "Agile Methodologies"]
+
+  export const testimonials = [
+    {
+        quote: 
+        "Erin has been the consummate professional during our time working together. She took over her role from another analyst and successfully replaced her predecessor with no loss of quality by the overall project team. She was able to become well-versed on a highly visible software product which we market for external clients, to the point where she was able to serve as Product Owner when needed. The external client is known as a singularly challenging one, and Erin’s product knowledge, initiative, commitment to quality, and overall professionalism was critical to our project team receiving a 5 (out of 5)-star rating for the project. Erin Ponsonby’s performance exemplifies her dedication, focus, and perseverance. Her work was integral to the company’s ability to provide stellar results for our clients. I would work with her on any project without hesitation.",
+        image: "/James-Greene.jpeg",
+        name: "James Greene",
+        company: "Excella",
+      },
+
+      {
+        quote: 
+        "Erin is one of those people that you rarely have the pleasure of meeting. In the 5 years we’ve known each other, we’ve worked on several projects together. We started a student organization while in university, managed a conference, and teamed up to compete in an international competition (which we won!). In every situation, Erin has shown strong leadership, dedication and a knack for creative problem solving. Her exceptional analytical skills coupled well with her interpersonal communication skills make her an exceptional leader. She would be a huge asset to any team she joins. I hope we have the opportunity to work together again in the future.",
+        image: "/Jailene-Santana.jpeg",
+        name: "Jailene Santana",
+        company: "Thought for Food",
+      },
+
+
+  ]
