2937590e12f4358171cd425427d35696b20f2963
Erin Ponsonby <erinponsonby@gmail.com>
2022-12-29 08:37:01 -0700

    Add resume links

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,21 +3,26 @@ import { DocumentTextIcon } from "@heroicons/react/24/solid";
 
 
 export default function Resume() {
+    const ResumeLink = ({className = '', children}) => (
+    <a
+        href="https://drive.google.com/file/d/1SexfemshVXR_5fFLfybJ893sJcugZv-v/view?usp=sharing"
+        target="_blank"
+      rel="noreferrer"
+      className={'font-medium ' + className}
+    >{children}</a>)
+
     return (
     <section id="resume">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
-          <DocumentTextIcon className="w-10 text-gray-900 inline-block mb-4" />
+          <ResumeLink><DocumentTextIcon className="w-10 text-gray-900 inline-block mb-4" /></ResumeLink>
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-gray-900 mb-4">
-            Resume
+            <ResumeLink>Resume</ResumeLink>
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto text-gray-900">
-            To see a copy of my resume, <a
-              href="https://drive.google.com/file/d/1SexfemshVXR_5fFLfybJ893sJcugZv-v/view?usp=sharing"
-            target="_blank"
-            rel="noreferrer"
+            A copy of my resume can be found <ResumeLink
             className="font-medium"
-            >click here</a>
+            >here</ResumeLink>
           </p>
         </div>
        </div>
